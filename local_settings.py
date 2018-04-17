a082683f11ef20459608dc9b86d3e5445e50499d
alexy <od-5@yandex.ru>
2018-04-17 16:01:55 +0300

    add file

---
+++ local_settings.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+LOCAL_SETTINGS = True
+from settings import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+ADMINS = (('Alexey', 'od-5@yandex.ru'),)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+CAM_SECURE_KEY = 'secret'
+STREAM_ROOT = '/tmp/hls'
+CMD_KEY = '490683'
+
