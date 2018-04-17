1fa1e66b82e9e4850b6b0bff174b25c50692d14a
alexy <od-5@yandex.ru>
2018-04-17 15:08:42 +0300

    fix script

--- script_stream.sh
+++ script_stream.sh
@@ -3,6 +3,7 @@ echo 'add user alexy'
 adduser alexy
 usermod -aG sudo alexy
 su alexy
+cd /home/alexy/
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3
 sudo apt-get update
 sudo apt-get -y install libpcre3 libpcre3-dev libssl-dev unzip gcc make ffmpeg libav-tools x264 x265 git python-dev python-pip supervisor
@@ -20,8 +21,6 @@ cp ../nginx-rtmp-module-master/arut-nginx-rtmp-module-43f1e42/stat.xsl /etc/ngin
 echo '---- install packet ----'
 cd /home/alexy/
 mkdir stream stream/static stream/media/ stream/log stream/supervisor stream/supervisor/logs
-wget https://github.com/od-5/nginx-rtmp-config/archive/master.zip
-unzip master.zip
 cp nginx-rtmp-config-master/uwsgi.ini stream/
 cp nginx-rtmp-config-master/production.conf stream/supervisor
 sudo cp nginx-rtmp-config-master/nginx.conf /etc/nginx/
